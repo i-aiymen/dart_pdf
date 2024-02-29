4c280a84c24420373be2cbb5ecf84869f576732d
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2024-02-29 09:15:32 +0900

    Updated: Pdf display format

---
+++ demo/lib/app.dart
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2017, David PHAM-VAN <dev.nfet.net@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+import 'examples/resume.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  PrintingInfo? printingInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  Future<void> _init() async {
+    final info = await Printing.info();
+
+    setState(() {
+      printingInfo = info;
+    });
+  }
+
+  void _showPrintedToast(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Document printed successfully'),
+      ),
+    );
+  }
+
+  void _showSharedToast(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Document shared successfully'),
+      ),
+    );
+  }
+
+  Future<void> _saveAsFile(
+    BuildContext context,
+    LayoutCallback build,
+    PdfPageFormat pageFormat,
+  ) async {
+    final bytes = await build(pageFormat);
+
+    final appDocDir = await getApplicationDocumentsDirectory();
+    final appDocPath = appDocDir.path;
+    final file = File('$appDocPath/document.pdf');
+    print('Save as file ${file.path} ...');
+    await file.writeAsBytes(bytes);
+    await OpenFile.open(file.path);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    pw.RichText.debug = true;
+
+    final actions = <PdfPreviewAction>[
+      if (!kIsWeb)
+        PdfPreviewAction(
+          icon: const Icon(Icons.save),
+          onPressed: _saveAsFile,
+        )
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter PDF Demo'),
+      ),
+      body: PdfPreview(
+        maxPageWidth: 700,
+        build: (format) => generateResume(
+          format,
+        ),
+        actions: actions,
+        onPrinted: _showPrintedToast,
+        onShared: _showSharedToast,
+      ),
+    );
+  }
+}
